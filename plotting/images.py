ad7286a6846c63a1214ee70ae6de973a1add81ba
Maksim <maksim.pz.leon@gmail.com>
2026-02-13 17:46:02 +0300

    Refactor image building functions to use Plotly

--- plotting/images.py
+++ plotting/images.py
@@ -1,37 +1,59 @@
-import matplotlib.pyplot as plt
-from .draw import draw_dimension, draw_notation, draw_point, draw_shape
-from matplotlib.patches import Patch
+import plotly.graph_objects as go
+from .draw import (
+    draw_shape,
+    draw_point,
+    draw_dimension,
+    draw_notation,
+)
+def apply_layout(fig):
+    fig.update_layout(
+        xaxis=dict(scaleanchor="y", visible=False, constrain="domain"),
+        yaxis=dict(visible=False, constrain="domain"),
+        legend=dict(orientation="h", yanchor="top", y=0.0, xanchor="left", x=0.0, font=dict(size=16)),
+        margin=dict(l=20, r=20, t=20, b=20)
+    )
 
-def build_image_1(show_dimension: bool, show_notation: bool,
-                  params: dict, shape_nodes, center):
-    fig, ax = plt.subplots(figsize=(6,6))
-    style = {"facecolor": "#f0f2f6", "edgecolor": "black"}
-    draw_shape(ax, shape_nodes, style)
+def build_image_1(show_dimension, params, shape_nodes, center):
+    fig = go.Figure()
+
+    draw_shape(fig, shape_nodes, {"facecolor": "#f0f2f6", "edgecolor": "black"})
+
     if show_dimension:
-        draw_dimension(ax, params)
-    if show_notation:
-        draw_notation(ax, params)
-    draw_point(ax, center, {"marker":"+","color":"grey","label":"Центр тяжести"})
-    ax.legend(loc="lower left", bbox_to_anchor=(0,-0.15), frameon=False)
-    ax.set_aspect('equal')
-    ax.axis("off")
+        draw_dimension(fig, params)
+    else:
+        draw_notation(fig, params)
+
+    draw_point(fig, center, {
+        "marker": dict(size=9, symbol="cross-thin", line=dict(width=2, color="grey")),
+        "label": "Центр тяжести",
+    })
+
+    apply_layout(fig)
     return fig
 
-def build_image_2(shape_nodes, red_nodes, center, red_center, force_node):
-    fig, ax = plt.subplots(figsize=(6,6))
-    draw_shape(ax, shape_nodes, {"facecolor":"#E3533680","edgecolor":"none"})
-    draw_shape(ax, red_nodes, {"facecolor":"#f0f2f6","edgecolor":"none"})
-    draw_shape(ax, shape_nodes, {"facecolor":"none","edgecolor":"black"})
-    draw_point(ax, center, {"marker":"+","color":"grey","label":"Центр тяжести"})
-    draw_point(ax, red_center, {"marker":"+","color":"black","label":"Центр тяжести сжатой зоны"})
-    draw_point(ax, force_node, {"marker":".","color":"#E35336","label":"Точка приложения силы"})
-    # Легенда
-    handles, labels = ax.get_legend_handles_labels()
-    handles.append(Patch(facecolor="#f0f2f6", edgecolor="none"))
-    labels.append("Сжатая зона")
-    handles.append(Patch(facecolor="#E3533680", edgecolor="none"))
-    labels.append("Растянутая зона")
-    ax.legend(handles=handles, labels=labels, loc="lower left", bbox_to_anchor=(0,-0.15), frameon=False)
-    ax.set_aspect('equal')
-    ax.axis("off")
+def build_image_2(params, shape_nodes, red_nodes, center, red_center, force_node):
+    fig = go.Figure()
+
+    draw_shape(fig, shape_nodes, {"facecolor": "#d6705c", "edgecolor": None})
+    draw_shape(fig, red_nodes, {"facecolor": "#f0f2f6", "edgecolor": None})
+    draw_shape(fig, shape_nodes, {"facecolor": "rgba(0,0,0,0)", "edgecolor": "black"})
+
+    draw_point(fig, center, {
+        "marker": dict(size=9, symbol="cross-thin", line=dict(width=2, color="grey")),
+        "label": "Центр тяжести",
+    })
+
+    draw_point(fig, red_center, {
+        "marker": dict(size=9, symbol="x-thin", line=dict(width=2, color="grey")),
+        "label": "Центр тяжести сжатой зоны",
+    })
+
+    draw_point(fig, force_node, {
+        "marker": dict(size=8, symbol="circle-open", color="#d6705c", line=dict(width=2)),
+        "label": "Точка приложения силы",
+    })
+
+    draw_dimension(fig, params)
+
+    apply_layout(fig)
     return fig
